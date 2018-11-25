50ff62b141622343fe6cda7e0b79e8e162325fd2
cosychair <37767339+cosychair@users.noreply.github.com>
2018-11-25 19:44:54 +0300

    Just a template for tests3

---
+++ TestForLab3/TestForLab3.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestForLab3
+{		
+	TEST_CLASS(BinaryTreeTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Разместите здесь код своего теста
+		}
+
+	};
+}
